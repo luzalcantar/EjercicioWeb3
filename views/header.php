128bac03ed87b95acfad19c1347398c4f58639fd
luzalcantar <luzalcantar1303@gmail.com>
2021-02-28 18:35:43 -0700

    hotfix/Cambio de Titulo

--- views/header.php
+++ views/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicio Web 2</title>
+    <title>Ejercicio Web 3</title>
 
     <!-- CSS -->
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
